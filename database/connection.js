d274b4a1eb3dcb8ab0d30deb425a41af64adb178
upthegraph <dontstrealfrompoor@protonmail.com>
2024-07-05 13:06:44 +0530

    Loan-Management: code updated

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const url = process.env.NODE_ENV === "production" ? 
 process.env.PROD_MONGO_URL : process.env.DEV_MONGO_URL;
 
-mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('strictQuery', false);
 
 const db = mongoose.connect(url, {
+    dbName: process.env.DB_NAME,
     useNewUrlParser: true, 
     useUnifiedTopology: true
     
